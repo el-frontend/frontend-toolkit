f61f286c3dcf6e9ab90c97a304b409da616674eb
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-10-30 08:50:44 -0400

    feat: update npm configuration

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -11,9 +11,24 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    ["@semantic-release/git", {
-      "message": "chore(release): ${nextRelease.version} [skip ci]"
-    }]
-  ]  
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "dist",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+  ],
 };
